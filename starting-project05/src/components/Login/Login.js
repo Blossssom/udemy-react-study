f94b4e26fc57d92cfa163f6a3598d3ac087f9489
Blossssom <eju2003@gmail.com>
2022-07-25 01:57:19 +0900

    edit useRef, forwardRef, useImperactiveHandle

--- starting-project05/src/components/Login/Login.js
+++ starting-project05/src/components/Login/Login.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useRef, useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import AuthContexts from '../../store/authContext';
+import Input from '../UI/Input/Input';
 
 const emailReducer = (state, action) => {
   if(action.type === "USER_INPUT") {
@@ -32,11 +33,27 @@ const passwordReducer = (state, action) => {
   }
 };
 
+const nicknameReducer = (state, action) => {
+  switch(action.type) {
+    case "USER_NICKNAME":
+      return {value: action.val, isValid: action.val.trim().length > 4}
+    case "NICKNAME_BLUR":
+      return {value: state.value, isValid: state.value.trim().length > 4}
+    default:
+      return;
+  }
+}
+
 const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {value: '', isValid: null});
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: null});
+  const [nicknameState, dispatchNickname] = useReducer(nicknameReducer, {value: '', isValid: null});
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const nicknameInputRef = useRef();
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -48,6 +65,7 @@ const Login = (props) => {
 
   const { isValid: emailValid } = emailState;
   const { isValid: passwordValid } = passwordState;
+  const { isValid: nicknameValid } = nicknameState;
   // 디스트럭쳐링 => 유효성 검사가 끝난 이후엔 리렌더링을 막기위해 지정
 
   const authCtx = useContext(AuthContexts);
@@ -56,7 +74,7 @@ const Login = (props) => {
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
       setFormIsValid(
-        emailValid && passwordValid
+        emailValid && passwordValid && nicknameValid
       );
     }, 500);
 
@@ -64,23 +82,23 @@ const Login = (props) => {
       console.log('CLEANUP');
       clearTimeout(identifier);
     };
-  }, [emailValid, passwordValid]);
+  }, [emailValid, passwordValid, nicknameValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
 
-    setFormIsValid(
-      // event.target.value.includes('@') && enteredPassword.trim().length > 6
-      emailState.isValid && passwordState.isValid
-    );
+    // setFormIsValid(
+    //   // event.target.value.includes('@') && enteredPassword.trim().length > 6
+    //   emailValid && passwordValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({type: 'USER_PW', val: event.target.value});
+  };
 
-    setFormIsValid(
-      emailState.isValid && passwordState.isValid
-    );
+  const nicknameChangeHandler = (event) => {
+    dispatchNickname({type: "USER_NICKNAME", val: event.target.value});
   };
 
   const validateEmailHandler = () => {
@@ -93,44 +111,32 @@ const Login = (props) => {
     dispatchPassword({type: 'PW_BLUR'});
   };
 
+  const validateNicknameHandler = () => {
+    dispatchNickname({type: "NICKNAME_BLUR"});
+  };
+
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if(formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    }else if(!emailValid){
+      emailInputRef.current.focus();
+    }else if(!passwordValid) {
+      passwordInputRef.current.focus();
+    }else {
+      nicknameInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input ref={emailInputRef} ids='email' label='E-Mail' type='email' isValid={emailValid} changeHandler={emailChangeHandler} validateHandler={validateEmailHandler} value={emailState.value} />
+        <Input ref={passwordInputRef} ids='password' label='Password' type='password' isValid={passwordValid} changeHandler={passwordChangeHandler} validateHandler={validatePasswordHandler} value={passwordState.value} />
+        <Input ref={nicknameInputRef} ids='nickname' label='NickName' type='text' isValid={nicknameValid} changeHandler={nicknameChangeHandler} validateHandler={validateNicknameHandler} value={nicknameState.value} />
+        
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
---
+++ starting-project05/src/components/UI/Input/Input.jsx
@@ -0,0 +1,33 @@
+import React, {useRef, useImperativeHandle} from 'react'
+import classes from './Input.module.css';
+
+const Input = React.forwardRef(({isValid, type, changeHandler, validateHandler, value, label, ids}, ref) => {
+
+    const inputRef = useRef();
+
+    const activate = () => {
+        inputRef.current.focus();
+    };
+
+    useImperativeHandle(ref, () => {
+        return {
+           focus: activate 
+        } ;
+    });
+
+  return (
+    <div className={`${classes.control} ${isValid === false ? classes.invalid : ''}`}>
+        <label htmlFor={ids}>{label}</label>
+        <input
+            ref={inputRef}
+            id={ids}
+            type={type}
+            onChange={changeHandler}
+            onBlur={validateHandler}
+            value={value}
+        />
+    </div>
+  )
+})
+
+export default Input;
