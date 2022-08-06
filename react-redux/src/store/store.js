be32434eaa681935150ff3cb715fed535409fe96
Blosssom <eju2003@gmail.com>
2022-08-06 17:01:25 +0900

    study react-redux

--- react-redux/src/store/store.js
+++ react-redux/src/store/store.js
@@ -1,15 +1,59 @@
 import {createStore} from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
-const counterReducer = (state = {counter: 0}, action) => {
-    if(action.type === 'INCREMENT') {
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const INCREASE = 'INCREASE';
+export const TOGGLESHOW = 'TOGGLESHOW';
+
+const initState = {counter: 0, toggleShow: true};
+
+createSlice({
+    name: 'counter',
+    // slice의 식별자 이름은 임의로 정할 수 있다.,
+    initialState: initState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter += action.amount;
+        },
+        toggleShow(state) {
+            state.toggleShow = !state.toggleShow;
+        }
+    }
+})
+
+const counterReducer = (state = initState, action) => {
+    if(action.type === INCREMENT) {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            toggleShow: state.toggleShow
         }
     }
 
-    if(action.type === 'DECREMENT') {
+    if(action.type === INCREASE) {
         return {
-            counter: state.counter - 1
+            counter: state.counter + action.amount,
+            toggleShow: state.toggleShow
+        }
+    }
+
+    if(action.type === DECREMENT) {
+        return {
+            counter: state.counter - 1,
+            toggleShow: state.toggleShow
+        }
+    }
+
+    if(action.type === TOGGLESHOW) {
+        return {
+            counter: state.counter,
+            toggleShow: !state.toggleShow
         }
     }
     
